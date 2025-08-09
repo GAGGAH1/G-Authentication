49506ba5d4c2b1aa4124a73282dbae38b3e95457
GAGGAH1 <titusdcstd2022@gmail.com>
2025-08-09 22:11:41 +0100

    Hiding Navbar and Footer for the Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import EmailVerify from './pages/EmailVerify'   
@@ -8,9 +8,12 @@ import Footer from './components/Footer'
 import NotFound from './pages/NotFound'
 
 const App = () => {
+
+  const location = useLocation()
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
+      {location.pathname !== '/login' && <Navbar />}
        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
@@ -18,7 +21,8 @@ const App = () => {
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="*" element={<NotFound />} />
        </Routes>
-      <Footer />
+      {/* <Footer /> */}
+      {location.pathname !== '/login' && <Footer />}
        
     </>
   )
