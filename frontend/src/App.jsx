79e8de362d4e5b7daa52abb053312ea316c8e43d
GAGGAH1 <titusdcstd2022@gmail.com>
2025-08-15 20:27:47 +0100

    Made changes with register component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,23 +6,28 @@ import ResetPassword from './pages/ResetPassword'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import NotFound from './pages/NotFound'
+import Register from './pages/Register'
 
+const hiddenPaths = ['/login','/register', '/email-verify', '/reset-password']
 const App = () => {
-
   const location = useLocation()
+  const hideLayout = hiddenPaths.includes(location.pathname)
   return (
     <>
       {/* <Navbar /> */}
-      {location.pathname !== '/login' && <Navbar />}
+      {!hideLayout && <Navbar />}
+      
        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/email-verify" element={<EmailVerify />} />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="*" element={<NotFound />} />
        </Routes>
       {/* <Footer /> */}
-      {location.pathname !== '/login' && <Footer />}
+      {!hideLayout && <Footer />}
+      
        
     </>
   )
